6f32c3fb00a5f69849129d7523f199b78b071a65
Rafael Queiroz Santos <rafaelqsantos33@gmail.com>
2025-11-14 11:55:28 -0300

    Initial commit

---
+++ app/src/main/java/com/offauth/app/app/OffAuthApp.kt
@@ -0,0 +1,11 @@
+package com.offauth.app.app
+
+import androidx.compose.runtime.Composable
+import androidx.compose.material3.MaterialTheme
+
+@Composable
+fun OffAuthApp() {
+    MaterialTheme {
+        // NavHost entrará aqui nos próximos passos
+    }
+}
